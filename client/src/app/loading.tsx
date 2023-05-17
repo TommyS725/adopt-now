9b616b53f3dcdf1836b3fcf78cbb8e4ee846e778
TommyS725 <120657256+TommyS725@users.noreply.github.com>
2023-05-18 01:05:36 +0800

    Update loading.tsx

--- client/src/app/loading.tsx
+++ client/src/app/loading.tsx
@@ -1,4 +1,6 @@
-import { LoadingAnimation } from "@/components/ui/loadingAnimation"
+import { ScrollTop } from '@/components/ui/BackToTop'
+import {HeaderSlide} from '@/components/Swiper'
+import { MainSection } from "@/components/MainSection"
 
 
 export default async function Home() {
@@ -7,9 +9,13 @@ export default async function Home() {
     return (
       <>
       <main className="flex min-h-screen flex-col items-center  w-full h-full px-2 py-4 ">
-        <LoadingAnimation size={100}/>
+        <div className=" py-4 w-full">
+          <HeaderSlide/>
+        </div>
+        <h1 className='py-4'>Save lifessssss!</h1>
+        <MainSection providers={[]}/>
+      <ScrollTop/>
       </main>
-      
       </>
     )
   }
